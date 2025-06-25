647d93214b74870350bea0ffd04900fa738907a0
@maden <166928262+maden77@users.noreply.github.com>
2025-06-25 16:16:01 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,29 @@
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open('stack-builder-v1').then(cache => {
-      return cache.addAll(['.', 'index.html', 'manifest.json', 'icons/icon2048-192x192.png']);
-    })
+const CACHE_NAME = 'stack-builder-v1';
+const ASSETS_TO_CACHE = [
+  '.',
+  'index.html',
+  'manifest.json',
+  // tambahkan file ikon yang diperlukan
+  'icons/icon2048-72x72.png',
+  'icons/icon2048-96x96.png',
+  'icons/icon2048-128x128.png',
+  'icons/icon2048-144x144.png',
+  'icons/icon2048-152x152.png',
+  'icons/icon2048-192x192.png',
+  'icons/icon2048-384x384.png',
+  'icons/icon2048-512x512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS_TO_CACHE))
   );
 });
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(response => response || fetch(e.request))
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(resp => {
+      return resp || fetch(event.request);
+    })
   );
 });

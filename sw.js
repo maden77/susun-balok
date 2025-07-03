d668ddfe8c3e9a84616a3ebc78647bece10091b2
@maden <166928262+maden77@users.noreply.github.com>
2025-07-03 20:13:16 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,29 +1,22 @@
-const CACHE_NAME = 'stack-builder-v1';
-const ASSETS_TO_CACHE = [
-  '.',
-  'index.html',
-  'manifest.json',
-  // tambahkan file ikon yang diperlukan
-  'icons/icon2048-72x72.png',
-  'icons/icon2048-96x96.png',
-  'icons/icon2048-128x128.png',
-  'icons/icon2048-144x144.png',
-  'icons/icon2048-152x152.png',
-  'icons/icon2048-192x192.png',
-  'icons/icon2048-384x384.png',
-  'icons/icon2048-512x512.png'
+const CACHE_NAME = 'blockcraft-v1';
+const ASSETS = [
+    '/',
+    '/index.html',
+    '/icon-192.png',
+    '/icon-512.png',
+    '/manifest.json'
 ];
 
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS_TO_CACHE))
-  );
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(ASSETS))
+    );
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(resp => {
-      return resp || fetch(event.request);
-    })
-  );
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => response || fetch(event.request))
+    );
 });
